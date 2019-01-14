187cd95c0247d15c26b8696341f3ab1fa7ae9f8f
Pratim Sarkar <pratimsarkar23@gmail.com>
2019-01-15 01:20:00 +0530

    Add solution for problem `A - Maximum Increase` Link: https://codeforces.com/contest/702/problem/A Tags: dp, greedy, implementation, *800 Ref: https://codeforces.com/contest/702/submission/48399532

---
+++ codeforces/702/A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll int
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ll n,i,j,ctr=0,max=-1;
+	cin>>n;
+	ll ar[n];
+	for(i=0;i<n;i++)
+	//scanf("%d",&ar[i]);
+	cin>>ar[i];
+	if(n==1)
+	{
+		cout<<1;
+		return 0;
+	}
+	for(i=0;i<n-1;i++)
+	{
+		ctr=0;
+		j=i;
+		while(ar[i]<ar[i+1]&&i<n-1)
+		{
+			i++;
+			ctr++;
+		}
+		if(max<ctr)
+		{
+			max=ctr;
+			i--;
+		}
+	}
+	//if(max>0)
+	cout<<max+1;
+	//else
+	//cout<<max;
+}
