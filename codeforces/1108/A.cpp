30a8d05a7950c12adbee30a1bc11d41ecb3dbc21
Pratim Sarkar <pratimsarkar23@gmail.com>
2019-01-23 20:16:00 +0530

    Add solution for problem `A - Two distinct points` Link: https://codeforces.com/contest/1108/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1108/submission/48808795

---
+++ codeforces/1108/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int l1,r1,l2,r2,t;
+	cin>>t;
+	while(t--)
+	{
+		cin>>l1>>r1>>l2>>r2;
+		if(l1<=l2)
+		{
+			cout<<l1<<' '<<r2<<endl;
+		}
+		else
+		{
+			cout<<r1<<' '<<l2<<endl;
+		}
+	}
+}
