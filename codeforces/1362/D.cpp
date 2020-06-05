76339d3e8d66769e49b7dd3276556be6c727bdd2
Pratim Sarkar <pratimsarkar23@gmail.com>
2020-06-05 10:08:00 +0530

    Add solution for problem `D - Johnny and Contribution` Link: https://codeforces.com/contest/1362/problem/D Tags: constructive algorithms, graphs, greedy, implementation, sortings, *1700 Ref: https://codeforces.com/contest/1362/submission/82594511

---
+++ codeforces/1362/D.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+	ll i,j,k,m,n,t;
+	cin>>n>>m;
+	ll ar[n+1];
+	vector<pair<ll,ll>> v;
+	list<ll> adj[n+1];
+	for(i=0;i<m;i++)
+	{
+		cin>>j>>k;
+		adj[j].push_back(k);
+		adj[k].push_back(j);
+	}
+	for(i=1;i<=n;i++)
+	{
+		cin>>ar[i];
+		v.push_back({ar[i],i});
+	}
+	sort(v.begin(),v.end());
+	ll ans[n+1]={0};
+	bool flag=true;
+	for(i=0;i<n;i++)
+	{
+		set<ll> st;
+		for(auto x:adj[v[i].second])
+		{
+			if(ans[x]!=0)
+				st.insert(ans[x]);
+			if(ans[x]==ar[v[i].second])
+			{
+				cout<<-1;
+				return 0;
+			}
+		}
+		if(st.size()!=ar[v[i].second]-1)
+		{
+			flag=false;
+			break;
+		}
+		else ans[v[i].second]=ar[v[i].second];
+	}
+	if(!flag) cout<<-1;
+	else
+	{
+		for(i=0;i<n;i++)
+		cout<<v[i].second<<' ';
+	}
+}
