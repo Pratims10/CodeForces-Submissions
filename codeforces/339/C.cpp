e485a402450a05bed391ae145b5bdeaa29304b05
Pratim Sarkar <pratimsarkar23@gmail.com>
2020-06-19 22:03:00 +0530

    Add solution for problem `C - Xenia and Weights` Link: https://codeforces.com/contest/339/problem/C Tags: constructive algorithms, dfs and similar, dp, graphs, greedy, shortest paths, *1700 Ref: https://codeforces.com/contest/339/submission/84346083

--- codeforces/339/C.cpp
+++ codeforces/339/C.cpp
@@ -3,26 +3,24 @@ using namespace std;
 #define ll long long int
 string str;
 ll n,dp[1001][21][11];
-ll ar[1001];
 ll func(ll ind,ll diff,ll prv)
 {
-	if(ind==n)
-	{
-		cout<<"YES\n";
-		for(ll i=0;i<n;i++) cout<<ar[i]<<' ';
-		exit(0);
-	}
-	if(dp[ind][diff][prv]!=-1) return dp[ind][diff][prv];
+	if(ind==n) return dp[ind][diff+10][prv]=1;
+	if(dp[ind][diff+10][prv]!=-1) return dp[ind][diff+10][prv];
 	ll mx=0;
 	for(ll i=0;i<10;i++)
 	{
-		if(str[i]=='1' and i+1!=prv and i+1-diff>0)
+		if(str[i]=='1' and i+1!=prv)
 		{
-			ar[ind]=i+1;
-			mx=max(mx,func(ind+1,i+1-diff,i+1));
+			if(ind%2==0 and (diff+i+1)>0){
+				mx=max(mx,func(ind+1,diff+i+1,i+1));
+				//if(ind==2)	cout<<ind+1<<' '<<diff+i+1<<' '<<i+1<<"\t\t"<<func(ind+1,diff+i+1,i+1)<<endl;
+			}
+			else if(ind%2==1 and diff-1-i<0) mx=max(mx,func(ind+1,diff-i-1,i+1));
 		}
 	}
-	return dp[ind][diff][prv]=mx;
+	//if(ind==2) cout<<mx<<endl;
+	return dp[ind][diff+10][prv]=mx;
 }
 vector<ll> ans;
 void printop(ll ind,ll diff,ll prv)
@@ -30,11 +28,26 @@ void printop(ll ind,ll diff,ll prv)
 	if(ind==n) return;
 	for(ll i=0;i<10;i++)
 	{
-		if(dp[ind+1][i+1-diff][i+1]==1)
+		if(ind%2==0)
 		{
-			ans.push_back(i+1);
-			printop(ind+1,i+1-diff,i+1);
-			break;
+			//if(ind==2) cout<<ind+1<<' '<<diff+i+1<<' '<<i+1<<"\t"<<dp[ind+1][diff+i+1][i+1]<<endl;
+			if(dp[ind+1][diff+i+1+10][i+1]==1 and i+1!=prv)
+			{
+				ans.push_back(i+1);
+				//cout<<ind<<endl;
+				printop(ind+1,diff+i+1,i+1);
+				break;
+			}
+		}
+		else
+		{
+			if(dp[ind+1][diff-i-1+10][i+1]==1 and i+1!=prv)
+			{
+				ans.push_back(i+1);
+				//cout<<ind<<endl;
+				printop(ind+1,diff-i-1,i+1);
+				break;
+			}
 		}
 	}
 }
@@ -51,5 +64,13 @@ int main()
 		}
 	}
 	ll x=func(0,0,0);
-	cout<<"NO\n";
+	if(x==1)
+	{
+		//cout<<'d'<<dp[1][1][1]<<"d\n";
+		cout<<"YES\n";
+		printop(0,0,0);
+		for(auto q:ans)
+		cout<<q<<' ';
+	}
+	else cout<<"NO\n";
 }
