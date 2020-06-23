50c9e3943f2d945172a9952c98b653ca1d583bf4
Pratim Sarkar <pratimsarkar23@gmail.com>
2020-06-23 19:40:00 +0530

    Add solution for problem `A - FashionabLee` Link: https://codeforces.com/contest/1369/problem/A Tags: geometry, math, *800 Ref: https://codeforces.com/contest/1369/submission/84758661

---
+++ codeforces/1369/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+	ll i,j,k,m,n,t;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		if(n%4==0) cout<<"YES\n";
+		else cout<<"NO\n";
+	}
+}
