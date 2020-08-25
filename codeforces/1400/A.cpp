36c3debf33c9ece22da4c95ca36e32e2fba5659a
Pratim Sarkar <pratimsarkar23@gmail.com>
2020-08-25 20:08:00 +0530

    Add solution for problem `A - String Similarity` Link: https://codeforces.com/contest/1400/problem/A Tags: constructive algorithms, strings, *800 Ref: https://codeforces.com/contest/1400/submission/90909767

---
+++ codeforces/1400/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+	ll i,j,k,m,n,t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>n;
+		cin>>s;
+		for(i=0;i<n;i++)
+		cout<<s[n-1];
+		cout<<endl;
+	}
+}
